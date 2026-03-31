67b04f107fe890499bf78f121b017bcde62a85db
IsarUslu <110565260+IsarUslu@users.noreply.github.com>
2026-03-30 21:54:17 -0400

    Added GPS Folder, no mods needed.

---
+++ BackendSoftware/src/GPS/GPS.h
@@ -0,0 +1,55 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include "../Sensor.h"
+#include "../../Math/Vector.h"
+
+namespace astra
+{
+    class GPS : public Sensor
+    {
+    public:
+        virtual ~GPS();
+        virtual Vector<3> getPos() const;
+        virtual Vector<3> getVel() const; // NED frame
+        virtual int getFixQual() const;
+        virtual double getHeading() const;
+        virtual bool getHasFix() const;
+        virtual int update(double currentTime = -1) override;
+        virtual int begin() override;
+        virtual Vector<3> getDisplacement(Vector<3> origin) const;
+
+        virtual const char *getTimeOfDay() const;
+
+        virtual int8_t getHour() const;
+        virtual int8_t getMinute() const;
+        virtual int8_t getSecond() const;
+        virtual uint8_t getDay() const;
+        virtual uint8_t getMonth() const;
+        virtual uint16_t getYear() const;
+
+    protected:
+        GPS(const char *name = "GPS");
+        Vector<3> position; // latitude, longitude, alt(m)
+        Vector<3> velocity; // vN (m/s), vE (m/s), vD (m/s)
+        int fixQual = 0;    // number of satellite connections
+        bool hasFix;        // whether or not GPS is currently connected to >= 4 satellites
+        bool hasFirstFix;   // the first time it gets a fix
+        double heading = 0;
+
+        // Distance-related calculations
+        void calcInitialValuesForDistance();
+        double kx, ky;
+        double wrapLongitude(double val) const;
+        void findTimeZone();
+
+        char tod[9];
+
+        int8_t hr = 0, min = 0, sec = 0;
+        uint8_t day = 0, month = 0;
+        uint16_t year = 0;
+        int8_t hrOffset = 0;
+    };
+}
+
+#endif // GPS_H
