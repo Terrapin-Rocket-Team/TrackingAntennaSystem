5bc9bb77c6d7dbd5b8bb2d09959c80bfb3aa6410
anthonyprzyzycki <anthonyprzyzycki2@gmail.com>
2026-05-04 21:04:37 -0400

    review updates
    for sam m10q

--- BackendSoftware/src/Sensors/SAM-M10Q.cpp
+++ BackendSoftware/src/Sensors/SAM-M10Q.cpp
@@ -8,11 +8,26 @@ GPS::GPS() {
 
 GPS::GPS(String name, TwoWire &wirePort, u_int8_t address) : DataReporter(name) {
     this->wire = &wirePort;
-    this->address = address; //default i2c address for SAM-M10Q is 0x42
-    //because SAM is a data reporter, we want to add columns for all the data it can report,
-    //ex:  addColumn("%0.7f", &position.x(), "Lat (deg)");
-    //check astra for examples of how to use addColumn
-    //add time stuff (hr, min, sec, day, month, year) as well for easier debugging and health monitoring
+    this->address = address = 0x42; //default i2c address for SAM-M10Q is 0x42
+
+
+    // registering all vals that sam reports
+    addColumn("%0.7f", &position.x(), "Lat (deg)");
+    addColumn("%0.7f", &position.y(), "Lon (deg)");
+    addColumn("%0.2f", &position.z(), "Alt (m)");
+    
+    addColumn("%0.2f", &velocity.x(), "VelN (m/s)");
+    addColumn("%0.2f", &velocity.y(), "VelE (m/s)");
+    addColumn("%0.2f", &velocity.z(), "VelD (m/s)");
+
+    addColumn("%d", &fixQual, "SIV");
+    addColumn("%d", &hr, "Hour");
+    addColumn("%d", &min, "Min");
+    addColumn("%d", &sec, "Sec");
+    addColumn("%d", &day, "Day");
+    addColumn("%d", &month, "Month");
+    addColumn("%d", &year, "Year");
+
 
 }
 
@@ -24,6 +39,7 @@ GPS::~GPS(){
 int GPS::begin(){
     if (!sam_m10q.begin(*wire, address)){ // try to initialize SAM over i2c
         initialized = false;
+        health = false;
         return -1;
     }
 
@@ -32,9 +48,12 @@ int GPS::begin(){
     sam_m10q.setDynamicModel(DYN_MODEL_AIRBORNE4g);
     sam_m10q.setAutoPVT(true);
     sam_m10q.saveConfiguration();
+
     initialized = true;
+    health = true;
     return 0;
 }
+
 
 
 int GPS::update(double currentTime){
@@ -47,11 +66,14 @@ int GPS::update(double currentTime){
     position.x() = sam_m10q.getLatitude() / 10000000.0;
     position.y() = sam_m10q.getLongitude() / 10000000.0;
     position.z() = sam_m10q.getAltitude() / 1000.0;
+
     velocity.x() = sam_m10q.getNedNorthVel() / 1000.0;
     velocity.y() = sam_m10q.getNedEastVel() / 1000.0;
     velocity.z() = sam_m10q.getNedDownVel() / 1000.0;
-    heading = sam_m10q.getHeading();
+
+    heading = sam_m10q.getHeading() / 100000.0;
     fixQual = sam_m10q.getSIV();
+
     hr = sam_m10q.getHour();
     min = sam_m10q.getMinute();
     sec = sam_m10q.getSecond();
@@ -62,27 +84,29 @@ int GPS::update(double currentTime){
 
 
     hasFix = (fixQual >= 4); // update fix status
+    health = hasFix;
     if (hasFix && !hasFirstFix){
         hasFirstFix = true;
         calcInitialValuesForDistance(); // for kx/ky
     }
 
-    updateHealth(0, currentTime);
     return 0;
 }
 
 
-// distance helpers
 
 //meters per degree scalers at current altitude
 void GPS::calcInitialValuesForDistance(){ 
-    double latRad = position.x() * M_PI / 180.0; //this calculation is too simplistic, too many assumptions 
-    //the Earth's radius must be taken into accoutn because rememebr the gps is on the ground, not at the center of the Earth, 
-    //so the curvature of the Earth matters for how much distance corresponds to a degree of lat/lon
-    //check the article astra has on this to understand the math better, but this is a common approximation for small distances
+    double latRad = position.x() * M_PI / 180.0; 
 
-    ky = 111320.0;
-    kx = 111320.0 * cos(latRad);
+    const double a = 6378137.0; // semi major axis
+    const double b = 6356752.3; //semi minor axis
+
+    // meters per degree of latitude
+    ky = (M_PI / 180.0) * (pow(a*b, 2) / pow(pow(a * cos(latRad), 2) + pow(b * sin(latRad), 2), 1.5));
+
+    //meters per degree of longitude
+    kx = (M_PI / 180.0) * (pow(a, 2) / sqrt(pow(a * cos(latRad), 2) + pow(b * sin(latRad), 2))) * cos(latRad);
 }
 
 
@@ -105,26 +129,10 @@ double GPS::wrapLongitude(double val) const {
 // returns disp in meters from origin to current pos
 Vector<3> GPS::getDisplacement(Vector<3> origin) const {
     Vector<3> disp;
-    disp.x() = (position.x() - origin.x()) * ky; //multiple by ky or kx here
-    disp.y() = wrapLongitude(position.y() - origin.y()) * kx; //same issue here 
+    disp.x() = (position.x() - origin.x()) * ky;
+    disp.y() = wrapLongitude(position.y() - origin.y()) * kx; 
     disp.z() = position.z() - origin.z();
     return disp;
-}
-
-
-//timezone helper
-
-//utc hour offset from current longitude (approx)
-void GPS::findTimeZone(){ //don't even really need this, might as well just delete this
-    hrOffset = static_cast<int8_t>(round(position.y() / 15.0))
-}
-
-
-//health
-void GPS::updateHealth(int readErr, double currentTime){ //delete this, use the boolean health member variable instead and just set it to false
-    // if readErr != 0 or if the gps doesn't have a fix, and true otherwise. Then use this in the main loop to decide whether or not to update the gps data
-
-    DataReporter::updateHealth(readErr, currentTime); //there is no update health in data reporter, i did that on purpose
 }
 
 
@@ -132,13 +140,9 @@ void GPS::updateHealth(int readErr, double currentTime){ //delete this, use the 
 // returns hh:mm:ss str adjusted by the offset 
 const char *GPS::getTimeOfDay() const {
     static char buf[9];
-    int8_t localHr = hr + hrOffset;
-    localHr = static_cast<int8_t>(((localHr % 24) + 24) % 24);
-    snprintf(buf, sizeof(buf), "%02d:%02d:%02d", localHr, min, sec);
+    snprintf(buf, sizeof(buf), "%02d:%02d:%02d", hr, min, sec);
     return buf;
 }
-
-//where is the update and begin function?? 
 
 
 //getters
